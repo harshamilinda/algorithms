3a48688ab6298a452674f668ffb49d3733f7047c
harshamilinda <harshamilinda@gmail.com>
2019-03-13 12:27:27 +0800

    updates

--- src/main/js.java
+++ src/main/js.java
@@ -4,8 +4,9 @@ package main;
 public class js {
 
     public static void main(String[] args){
-       System.out.println("Hello");
-       System.out.println(findGCD(18,6));
+      // System.out.println("Hello");
+      //  System.out.println(findGCD(18,6));
+        new StringUtil().sortArray();
     }
 
 
